c029913718f1e1ce01146de0134b813a58f1e3cd
dwachmann <elbuea@gmx.de>
2009-10-28 16:14:38 +0100

    change sshproxy script

--- scripts/sshproxy.cmd
+++ scripts/sshproxy.cmd
@@ -1,3 +1,3 @@
 @echo off
 echo call parameter:%0 %1 %2 %3 %4 %5 %6 %7 %8 %9 >> C:\Projects\git\sshproxy.out
-plink -load github %1 %2 %3 %4 %5 %6 %7 %8 %9
+"C:\Program Files\putty\plink.exe" -load gitproxy -ssh -v "%1" %2 %3 %4 %5 %6 %7 %8 %9
